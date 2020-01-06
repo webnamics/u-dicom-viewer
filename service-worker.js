f53ef6156a9f9a0051ce195592e1ba39895b41f2
webnamics <webnamics@google.com>
2020-01-06 11:29:22 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/u-dicom-viewer/precache-manifest.27a3eb9d4d0400f9f1c4c6d146031ee9.js"
+  "/u-dicom-viewer/precache-manifest.65bf51eff0ce0212902f5ca97b277fcb.js"
 );
 
 self.addEventListener('message', (event) => {
