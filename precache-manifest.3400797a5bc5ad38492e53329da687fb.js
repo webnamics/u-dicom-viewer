8d47c3e469f0e59237b652633a59e4e2868296b0
webnamics <webnamics@google.com>
2020-05-13 09:46:21 +0200

    Updates

---
+++ precache-manifest.3400797a5bc5ad38492e53329da687fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1ec7f5f479ee5d6ca46d9dfe98bc67",
+    "url": "/u-dicom-viewer/index.html"
+  },
+  {
+    "revision": "a1382d5c27cd4b1f046d",
+    "url": "/u-dicom-viewer/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "da642e06be24c7a39d08",
+    "url": "/u-dicom-viewer/static/css/main.5b250232.chunk.css"
+  },
+  {
+    "revision": "a1382d5c27cd4b1f046d",
+    "url": "/u-dicom-viewer/static/js/2.6c85e348.chunk.js"
+  },
+  {
+    "revision": "da642e06be24c7a39d08",
+    "url": "/u-dicom-viewer/static/js/main.a395447c.chunk.js"
+  },
+  {
+    "revision": "47a51e2081b6eae16985",
+    "url": "/u-dicom-viewer/static/js/runtime~main.4ed3829d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/u-dicom-viewer/precache-manifest.5287859124ed665b87a8204d619f5dd7.js"
+  "/u-dicom-viewer/precache-manifest.3400797a5bc5ad38492e53329da687fb.js"
 );
 
 self.addEventListener('message', (event) => {
